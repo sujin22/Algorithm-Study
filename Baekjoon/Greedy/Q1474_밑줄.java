57150ac5f13ebf4aae4b6da58068f5b0b6f94bcd
sujin22 <aoghksj@naver.com>
2022-02-08 19:16:32 +0900

    Q1474 주석 추가

--- Baekjoon/Greedy/Q1474_밑줄.java
+++ Baekjoon/Greedy/Q1474_밑줄.java
@@ -26,15 +26,17 @@ public class Q1474_밑줄 {
         }
 
         int _min = m/(n-1);     //언더바 개수 최솟값
-        int addCnt = m%(n-1);  //언더바 하나 더 추가되어야 하는 위치 개수
+        int addCnt = m%(n-1);  //언더바 하나 더 추가되어야 하는 위치 개수(차이가 1을 초과하면 안되므로)
 
         //언더바 개수가 _min보다 하나 큰 위치가 있는 경우
-        String result = words[0];
+        String result = words[0];   //첫번째 단어로 시작
+
+        //두번쨰 단어부터 돌린다.
         for(int i = 1; i<n;i++){
             if(addCnt>0 && (words[i].charAt(0)>'_' || n-i==addCnt)){
                 //언더바 추가해야 할 위치가 남아있는데,
                 //첫글자가 대문자거나, (본인포함)남은 단어 수와 addCnt가 같으면
-                //언더바 하나 더 넣기
+                //_min보다 언더바 하나 더 넣기
                 result+=getUnderBarString(_min+1)+words[i];
                 addCnt--;
             } else{
